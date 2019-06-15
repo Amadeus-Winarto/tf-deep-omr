a01a69ff12b292d8ea0a047329b2f6720180527d
AmadeusW <41280211+Amadeus-Winarto@users.noreply.github.com>
2019-06-15 18:29:35 +0800

    Update ctc_training.py

--- ctc_training.py
+++ ctc_training.py
@@ -53,10 +53,10 @@ for epoch in range(max_epochs):
         # VALIDATION
         print ('Loss value at epoch ' + str(epoch) + ':' + str(loss_value))
         print ('Validating...')
-        print('Checking for Memory Error 1...")
+        print("Checking for Memory Error 1...")
 
         validation_batch, validation_size = primus.getValidation(params)
-        print('Checking for Memory Error 2...")
+        print("Checking for Memory Error 2...")
         
         val_idx = 0
         
@@ -76,7 +76,7 @@ for epoch in range(max_epochs):
                                   mini_batch_feed_dict)
     
             str_predictions = ctc_utils.sparse_tensor_to_strs(prediction)
-            print('Checking for Memory Error 3...")
+            print("Checking for Memory Error 3...")
     
 
             for i in range(len(str_predictions)):
