f7704b7c0740a06be55fd8aacf0b2fffa0e19dae
AmadeusW <41280211+Amadeus-Winarto@users.noreply.github.com>
2019-06-16 00:21:00 +0800

    Update ctc_training.py

--- ctc_training.py
+++ ctc_training.py
@@ -3,6 +3,7 @@ from primus import CTC_PriMuS
 import ctc_utils
 import ctc_model
 import argparse
+from statistics import mean
 
 import os
 
@@ -17,6 +18,7 @@ parser.add_argument('-set',  dest='set', type=str, required=True, help='Path to 
 parser.add_argument('-save_model', dest='save_model', type=str, required=True, help='Path to save the model.')
 parser.add_argument('-vocabulary', dest='voc', type=str, required=True, help='Path to the vocabulary file.')
 parser.add_argument('-semantic', dest='semantic', action="store_true", default=False)
+parser.add_argument('-frequency', dest='frequency', type=int, default=1, help='Frequency of printing')
 args = parser.parse_args()
 
 # Load primus
@@ -33,60 +35,54 @@ dropout = 0.5
 inputs, seq_len, targets, decoded, loss, rnn_keep_prob = ctc_model.ctc_crnn(params)
 train_opt = tf.train.AdamOptimizer().minimize(loss)
 
-
 saver = tf.train.Saver(max_to_keep=None)
 sess.run(tf.global_variables_initializer())
 
-step_size = 
+step_size = primus.getTrainingSize // params['batch_size']
+
 # Training loop
 for epoch in range(max_epochs):
-    batch = primus.nextBatch(params)
+    loss = []
+    for step in range(step_size):
+        batch = primus.nextBatch(params, mode = 'Train')
 
-    _, loss_value = sess.run([train_opt, loss],
-                             feed_dict={
-                                inputs: batch['inputs'],
-                                seq_len: batch['seq_lengths'],
-                                targets: ctc_utils.sparse_tuple_from(batch['targets']),
-                                rnn_keep_prob: dropout,
-                            })
+        _, loss_value = sess.run([train_opt, loss],
+                                 feed_dict={
+                                    inputs: batch['inputs'],
+                                    seq_len: batch['seq_lengths'],
+                                    targets: ctc_utils.sparse_tuple_from(batch['targets']),
+                                    rnn_keep_prob: dropout,
+                                })
+        loss.append(loss_value)
 
-    if epoch % 1000 == 0:
+    if epoch % args.frequency == 0:
         # VALIDATION
-        print ('Loss value at epoch ' + str(epoch) + ':' + str(loss_value))
+        print ('Loss value at epoch ' + str(epoch) + ':' + str(mean(loss_value)))
         print ('Validating...')
-        print("Checking for Memory Error 1...")
 
-        validation_batch, validation_size = primus.getValidation(params) #One Batch Only
-        print("Checking for Memory Error 2...")
-        
-        val_idx = 0
-        
+        val_step = primus.getValidationSize // params['batch_size']
         val_ed = 0
-        val_len = 0
-        val_count = 0
-            
         
-        mini_batch_feed_dict = {
-            inputs: validation_batch['inputs'][val_idx:val_idx+params['batch_size']],
-            seq_len: validation_batch['seq_lengths'][val_idx:val_idx+params['batch_size']],
-            rnn_keep_prob: 1.0            
-        }            
-                        
+        for step in range(val_step):
+            validation_batch = primus.nextBatch(params, mode = 'Train')
+            mini_batch_feed_dict = {
+                inputs: validation_batch['inputs'][val_idx:val_idx+params['batch_size']],
+                seq_len: validation_batch['seq_lengths'][val_idx:val_idx+params['batch_size']],
+                rnn_keep_prob: 1.0            
+            }
             
-        prediction = sess.run(decoded,
-                              mini_batch_feed_dict)
+            prediction = sess.run(decoded,
+                                  mini_batch_feed_dict)
 
-        str_predictions = ctc_utils.sparse_tensor_to_strs(prediction)
-        print("Checking for Memory Error 3...")
+            str_predictions = ctc_utils.sparse_tensor_to_strs(prediction)
+            
 
+            for i in range(len(str_predictions)):
+                val_ed += ctc_utils.edit_distance(str_predictions[i], validation_batch['targets'][val_idx+i])
+                val_len = val_len + len(validation_batch['targets'][i])
+                val_count = val_count + 1
 
-        for i in range(len(str_predictions)):
-            ed = ctc_utils.edit_distance(str_predictions[i], validation_batch['targets'][val_idx+i])
-            val_ed = val_ed + ed
-            val_len = val_len + len(validation_batch['targets'][val_idx+i])
-            val_count = val_count + 1
-
-        val_idx = val_idx + params['batch_size']
+            
     
         print ('[Epoch ' + str(epoch) + '])
         print('Edit Distance: " + str(1. * val_ed / val_count)
